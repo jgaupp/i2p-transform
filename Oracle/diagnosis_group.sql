92100c0914c84c8fde4a9b81306221c6264f790a
catechol <catechol@kumc.edu>
2018-03-15 17:14:34 -0500

    Changing status check for group operations

--- Oracle/diagnosis_group.sql
+++ Oracle/diagnosis_group.sql
@@ -11,4 +11,4 @@ commit
 insert into cdm_status (status, last_update, records, group_start, group_end) select 'diagnosis_group', sysdate, count(*), :patient_num_first, :patient_num_last
 from diagnosis where patid between cast(:patient_num_first as number) and cast(:patient_num_last as number)
 /
-select 1 from cdm_status where status = 'diagnosis_group' and group_start = :patient_num_first and group_end = :patient_num_last
+select 1 from cdm_status where status = 'diagnosis_group'
